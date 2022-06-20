55a2c11424b03660df089c49230b3cdc533e9f3f
jatinkumar02 <jatin.kumar02@nagarro.com>
2022-06-20 14:10:12 +0530

    changws

--- getjobs.groovy
+++ getjobs.groovy
@@ -6,7 +6,8 @@
 //     @Grab(group='org.jenkins-ci.main', module='jenkins-core', version='2.355')
 // )
 
-package org.jenkins-ci.main;
+package org.jenkins-ci.main
+;
 import jenkins.model.*;
 import hudson.model.*;
 
